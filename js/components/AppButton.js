4af4a9bf8599ac6988a5a7cc3822dc60b50a0ad9
ghongatevivek <ghongatevivek1212@gmail.com>
2024-02-24 15:40:07 +0530

    worked on components

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,11 +1,21 @@
 export default {
     template : `
-        <button>
+        <button
+        :class="{
+            'border rounded px-5 py-2' : true,
+            'bg-blue-200 hover:bg-blue-400' : buttonType === 'primary',
+            'bg-purple-200 hover:bg-purple-400' : buttonType === 'secoundary',
+        }">
             <slot/>
         </button>
     `,
-
-    mounted () {
-        alert("123");
-    }
+    props : {
+        buttonType : {
+            type : String,
+            default : 'primary', 
+        },
+    },
+    // mounted () {
+    //     alert("123");
+    // }
 }
